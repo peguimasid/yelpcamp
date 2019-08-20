a1a2d7da0a38ad660e112324929cb7cf66d8e6c7
Guilhermo Masid <guilhermomasid@gmail.com>
2019-08-20 22:34:11 +0000

    add mongoose.connect

--- app.js
+++ app.js
@@ -17,7 +17,12 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds");
 
   
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://Guilhermo:Ben10123@cluster0-t4dr0.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useCreateIndex: true}).then(() => {
+	console.log("Connected to DB!");
+}).catch(err => {
+	console.log("ERROR:", err.message);
+});
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
